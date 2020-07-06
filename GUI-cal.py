0a91d7a9a9ac45be1353a50d996aa080335c4a96
Keshav Purohit <201751021@iiitvadodara.ac.in>
2020-07-06 15:07:29 +0530

    some UI changes + some fixes

--- GUI-cal.py
+++ GUI-cal.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 screen = Tk()
 screen.title('Yantra')
-screen.geometry('280x310')
-
+screen.geometry('280x295')
+screen.configure(bg = 'gray')
 #function to enter values using buttons 
 def click(number):
 	global operator
@@ -37,58 +37,58 @@ value1.grid(row = 0, columnspan = 4)
 
 #--------1st row-----------
 
-k7 = Button(screen,text = '7', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(7))
+k7 = Button(screen,text = '7', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(7),activebackground='black', activeforeground ='white') 
 k7.grid(row = 1,column = 0)
 
-k8 = Button(screen,text = '8', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(8))
+k8 = Button(screen,text = '8', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(8),activebackground='black', activeforeground ='white')
 k8.grid(row = 1,column = 1)
 
-k9 = Button(screen,text = '9', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(9))
+k9 = Button(screen,text = '9', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(9),activebackground='black', activeforeground ='white')
 k9.grid(row = 1,column = 2)
 
-kadd = Button(screen,text = '+', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('+'))
+kadd = Button(screen,text = '+', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('+'),activebackground='black', activeforeground ='white')
 kadd.grid(row = 1,column = 3)
 
 #--------2nd row-----------
 
-k4 = Button(screen,text = '4', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(4))
+k4 = Button(screen,text = '4', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(4),activebackground='black', activeforeground ='white')
 k4.grid(row = 2,column = 0)
 
-k5 = Button(screen,text = '5', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(5))
+k5 = Button(screen,text = '5', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(5),activebackground='black', activeforeground ='white')
 k5.grid(row = 2,column = 1)
 
-k6 = Button(screen,text = '6', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(6))
+k6 = Button(screen,text = '6', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(6),activebackground='black', activeforeground ='white')
 k6.grid(row = 2,column = 2)
 
-ksub = Button(screen,text = '-', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('-'))
+ksub = Button(screen,text = '-', font=('arial',15,'bold'), bd = 5,padx=14,pady=7,command = lambda:click('-'),activebackground='black', activeforeground ='white')
 ksub.grid(row = 2,column = 3)
 
 #--------3rd row-----------
 
-k1 = Button(screen,text = '1', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(1))
+k1 = Button(screen,text = '1', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(1),activebackground='black', activeforeground ='white')
 k1.grid(row = 3,column = 0)
 
-k2 = Button(screen,text = '2', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(2))
+k2 = Button(screen,text = '2', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(2),activebackground='black', activeforeground ='white')
 k2.grid(row = 3,column = 1)
 
-k3 = Button(screen,text = '3', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(3))
+k3 = Button(screen,text = '3', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(3),activebackground='black', activeforeground ='white')
 k3.grid(row = 3,column = 2)
 
-kmul = Button(screen,text = 'x', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('*'))
+kmul = Button(screen,text = 'x', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('*'),activebackground='black', activeforeground ='white')
 kmul.grid(row = 3,column = 3)
 
 #--------4th row-----------
 
-kclear = Button(screen,text = 'C', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = clear)
+kclear = Button(screen,text = 'C', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = clear,activebackground='black', activeforeground ='white')
 kclear.grid(row = 4,column = 0)
 
-k0 = Button(screen,text = '0', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(0))
+k0 = Button(screen,text = '0', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click(0),activebackground='black', activeforeground ='white')
 k0.grid(row = 4,column = 1)
 
-keq = Button(screen,text = '=', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = equalsTo)
+keq = Button(screen,text = '=', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = equalsTo,activebackground='black', activeforeground ='white')
 keq.grid(row = 4,column = 2)
 
-kdiv = Button(screen,text = '÷', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('/'))
+kdiv = Button(screen,text = '÷', font=('arial',15,'bold'), bd = 5,padx=12,pady=7,command = lambda:click('/'),activebackground='black', activeforeground ='white')
 kdiv.grid(row = 4,column = 3)
 
 
